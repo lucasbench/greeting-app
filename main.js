f970b87e9fe3302c01092df3a7a62e5a40c24211
lucasbench <54080085+lucasbench@users.noreply.github.com>
2019-09-23 23:11:52 -0400

    create person and create child functions + bonus

--- main.js
+++ main.js
@@ -1,38 +1,59 @@
-var family = [];
-
-function createPerson (name, age, coder, role) {
+var family = []; //family array
+var children = []; // children array
+//create a person function 
+function createPerson (name, age, coder, role, children) {
 
     var person = {};
     person.name = name;
     person.age = age;
     person.coder = coder;
     person.role = role;
+    person.children = children;
     family.push(person);
 }
+//call the function to actually create a person
+createPerson("Luca", 40, true, "father", true);
+createPerson("Gabi", 35, false, "mother", true);
+createPerson("Max", 1.5, false, "son", false);
+createPerson("Manu", 65, false, "granMa", true);
+createPerson("Paolo", 68, false, "granPa", true);
 
-createPerson("Luca", 40, true, "father");
-createPerson("Gabi", 35, false, "mother");
-createPerson("Max", 1.5, false, "son");
-createPerson("Manu", 65, false, "granMa");
-createPerson("Paolo", 68, false, "granPa");
 
+// function that creates a child
+function createChild (name, age, sex, etnicity) {
+    var child = {};
+    child.name = name;
+    child.age = age;
+    child.sex = sex;
+    child.etnicity = etnicity;
+    children.push(child);
+}
+// call the function to create children
+createChild("Max", 2, "male", "latinItalian");
+createChild("Luca", 40, "male", "Italian");
+createChild("Gabi", 35, "female", "latin");
+
+//log the array on server
 console.log(family);
-/*
+console.log(children);
+
+//bonus exercise js first lesson
 var wines =[];
 
 function wineOptions (name, origin, age, color) {
 
-    var grape ={};
+    var grape = {};
     grape.name = name;
     grape.origin = origin;
     grape.age = age;
     grape.color = color;
     wines.push(grape);
 
-}``
+}
 
 wineOptions("Riesling", "Germany", 30, "white");
 wineOptions("Chardonnay", "Burgundy", 60, "white");
 wineOptions("Cabernet  Sauvignon", "France", 45, "red");
 wineOptions("Pinot Noir", "France", 40, "red");
-*/
+
+console.log(wines);
