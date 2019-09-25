94c16d94e8b1c9de0d345341cc72f99efd615337
lucasbench <54080085+lucasbench@users.noreply.github.com>
2019-09-25 11:53:17 -0400

    js exercise and bonus, conditionals
    Hi Sina, I don't know how to use terminal to clone the repo, I tried but does't work, I need help on that.
    I think the exercise is right but please let me know. Thank you so much buddy!

--- main.js
+++ main.js
@@ -1,17 +1,97 @@
-var family = []; //family array
-var children = []; // children array
-//create a person function 
-function createPerson (name, age, coder, role, children) {
 
-    var person = {};
-    person.name = name;
-    person.age = age;
-    person.coder = coder;
-    person.role = role;
-    person.children = children;
-    family.push(person);
-}
-//call the function to actually create a person
+
+
+    //create function with conditionals
+    function whoIsOlder(person1, person2) {
+        if (person1.age > person2.age) {
+            console.log("luca is younger then Ciak", person1.age - person2.age);
+        } else if 
+            (person1.age > myFriend.age){
+            console.log("Luca is older then Gigi", person1.age - myFriend.age)
+        } else 
+            (person2.age > myFriend.age) 
+            console.log("Ciak is older then Gigi", person2.age - myFriend.age)
+    }
+  
+    //create objects
+    var me = {
+        name : "Luca",
+        age : 40
+    }
+    var mySibling = {
+        name : "Ciak",
+        age : 42
+    }
+    var myFriend = {
+        name : "Gigi",
+        age : 33
+    }
+   
+      //call the function for the objects
+      whoIsOlder(me, mySibling);
+      whoIsOlder(me, myFriend);
+      whoIsOlder(mySibling, myFriend);
+
+        /* function addTwoNumbers (num1,num2) {
+        var result = num1 + num2;
+        return result;
+    };
+
+    var sum = addTwoNumbers(5, 30);
+    console.log(sum);
+
+    
+    function checkAge (age) {
+        if (age >= 21){
+            return "drink";
+        }else{
+            return "denied";
+        }
+        console.log(person.age);
+        
+    }
+    
+    var person = {
+        age: 21
+    }
+
+    var result = checkAge(person.age);
+    console.log (result);*/
+
+
+    /*  var me = {};
+        me.name = "Luca";
+        me.age = 40;
+        me.coder = true;
+        me.role = "father";
+        me.children = ["Max"];
+
+
+    var gabi = {};
+        gabi.name = "Gabi";
+        gabi.age = 35;
+        gabi.coder = false;
+        gabi.role = "mom";
+        gabi.children = ["Max"];
+
+
+    var max = {};
+        max.name = "max";
+        max.age = 2;
+        max.coder = false;
+        max.role = "son";
+        max.children = [];
+
+ 
+
+        var family = [me, gabi, max];
+
+    console.log(family);
+
+
+
+
+    call the function to actually create a person
 createPerson("Luca", 40, true, "father", true);
 createPerson("Gabi", 35, false, "mother", true);
 createPerson("Max", 1.5, false, "son", false);
@@ -56,4 +136,4 @@ wineOptions("Chardonnay", "Burgundy", 60, "white");
 wineOptions("Cabernet  Sauvignon", "France", 45, "red");
 wineOptions("Pinot Noir", "France", 40, "red");
 
-console.log(wines);
+console.log(wines); */
